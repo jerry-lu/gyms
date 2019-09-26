94e15246a4ce5fb0628e61b4c929865b10d1356f
Jerry <jerry.z.lu@gmail.com>
2019-09-26 02:01:01 -0400

    refactor engine.py and Event model

--- gyms/engine.py
+++ gyms/engine.py
@@ -1,6 +1,7 @@
 from gyms.models import Event
 from gyms.config import api_key
 from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
 import re
 import os
 import requests
@@ -14,43 +15,52 @@ def to_local(utc_datetime):
     return utc_datetime + offset
 
 
+def format_titles(name):  # returns name and info
+    in_name_info = ""
+    if name.endswith("Hours"):
+        name = name.replace(" Hours", "")
+    elif name.startswith("CLOSED - "):
+        name = name.replace("CLOSED - ", "")
+        in_name_info = "closed"
+    elif name.endswith(" - CLOSED"):
+        name = name.replace(" - CLOSED", "")
+        in_name_info = "closed"
+    elif name.endswith("-CLOSED"):
+        name = name.replace("-CLOSED", "")
+        in_name_info = "closed"
+
+    if name.startswith("Pottruck Court"):
+        name = "Pottruck Courts"
+    elif name.startswith("Pottruck Hours"):
+        name = "Pottruck"
+    elif name.startswith("Membership"):
+        name = "Membership Services"
+    return name, in_name_info
+
+
 def get_events():
     # insert the api key from teamup.com/api-keys/ 
-    resp = requests.get("https://teamup.com/ks13d3ccc86a21d29e/events", timeout=30, headers={
-        "Teamup-Token": api_key
-    })
+    resp = requests.get("https://teamup.com/ks13d3ccc86a21d29e/events", 
+                        timeout=30, headers={"Teamup-Token": api_key})
     resp.raise_for_status()
     raw_data = resp.json()
     for item in raw_data["events"]:
         # handles some funny business with how the names are formatted
-        name = item["title"]
-        in_name_info = ""
-        if name.endswith("Hours"):
-            name = name.replace(" Hours", "")
-        elif name.startswith("CLOSED - "):
-            name = name.replace("CLOSED - ", "")
-            in_name_info = "closed"
-        elif name.endswith(" - CLOSED"):
-            name = name.replace(" - CLOSED", "")
-            in_name_info = "closed"
-        elif name.endswith("-CLOSED"):
-            name = name.replace("-CLOSED", "")
-            in_name_info = "closed"
-
-        if name.startswith("Pottruck Court"):
-            name = "Pottruck Courts"
-        elif name.startswith("Pottruck Hours"):
-            name = "Pottruck"
-        elif name.startswith("Membership"):
-            name = "Membership Services"
+        name, in_name_info = format_titles(item["title"])
         all_day = item["all_day"]
         date = iso8601.parse_date(item["start_dt"])
         notes = item["notes"]
+
+        e, created = Event.objects.get_or_create(
+            name=name
+        )
+        e.all_day = all_day
+        e.date = date
+        e.in_name_info = in_name_info
+        e.notes = notes
         if not all_day:
             start = to_local(iso8601.parse_date(item["start_dt"]))
             end = to_local(iso8601.parse_date(item["end_dt"]))
-            e = Event(name=name, all_day=all_day, date=date, start=start, end=end, notes=notes)
-            e.save()
-        else:
-            e = Event(name = name, all_day = all_day, date = date, in_name_info=in_name_info, notes=notes)
-            e.save()
+            e.start = start
+            e.end = end
+        e.save()
--- gyms/models.py
+++ gyms/models.py
@@ -4,23 +4,26 @@ from datetime import datetime
 from datetime import timedelta
 import pytz
 
+
 class Event(models.Model):
     name = models.CharField(max_length=200)
-    all_day = models.BooleanField()
-    in_name_info = models.CharField(max_length=200, blank = True, null = True)
-    date = models.DateField(default = timezone.now)
-    start = models.DateTimeField(blank = True, null = True)
-    end = models.DateTimeField(blank = True, null = True)
+    all_day = models.BooleanField(default=True)
+    in_name_info = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateField(default=timezone.now)
+    start = models.DateTimeField(blank=True, null=True)
+    end = models.DateTimeField(blank=True, null=True)
     notes = models.CharField(max_length=500, null=True)
 
     def __str__(self):
-       return self.name
+        return self.name
+
     def open_now(self):
         if(not self.all_day):
             now = pytz.utc.localize(datetime.now())
             return now < self.end and now > self.start
         else:
             return self.in_name_info != "closed"
+
     def closing_soon(self):
         if(self.open_now()):
             now = pytz.utc.localize(datetime.now())
