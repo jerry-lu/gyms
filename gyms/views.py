1b203fce13677ce00c37fb04450d3ec6404892e2
Jerry <jerry.z.lu@gmail.com>
2019-10-06 15:42:34 -0400

    changed climbing wall pic and stopped deleting Events

--- gyms/views.py
+++ gyms/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from gyms.models import Event, EventTime
+from gyms.models import Event
 from gyms.engine import get_events
 
 
 def index(request):
-    Event.objects.all().delete()
     get_events()
     events = list(Event.objects.all().order_by('name'))
     events = sorted(events, key=lambda x: x.open_now, reverse=True)
